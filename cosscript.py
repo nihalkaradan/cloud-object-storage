aa5d8c7d737c4f19f8a5f1fca5af0dc89de4adcd
nihalkaradan <nihalkaradan@gmail.com>
2020-10-27 01:13:14 +0530

    Update cosscript.py

--- cosscript.py
+++ cosscript.py
@@ -4,8 +4,8 @@ from ibm_botocore.client import Config, ClientError
 
 # Constants for IBM COS values
 COS_ENDPOINT = "https://s3.us-south.cloud-object-storage.appdomain.cloud" # Current list avaiable at https://control.cloud-object-storage.cloud.ibm.com/v2/endpoints
-COS_API_KEY_ID = "3uzOySxV69_MGvCltDYjWRkCr-PejfERE6JIFYiAogUc" # eg "W00YixxxxxxxxxxMB-odB-2ySfTrFBIQQWanc--P3byk"
-COS_INSTANCE_CRN = "crn:v1:bluemix:public:cloud-object-storage:global:a/ba242e097299224620d429e0d2fe9b92:05649a80-4e35-4043-b97b-dda8e7eb3ceb::" # eg "crn:v1:bluemix:public:cloud-object-storage:global:a/3bf0d9003xxxxxxxxxx1c3e97696b71c:d6f04d83-6c4f-4a62-a165-696756d63903::"
+COS_API_KEY_ID = "" # eg "W00YixxxxxxxxxxMB-odB-2ySfTrFBIQQWanc--P3byk"
+COS_INSTANCE_CRN = "" # eg "crn:v1:bluemix:public:cloud-object-storage:global:a/3bf0d9003xxxxxxxxxx1c3e97696b71c:d6f04d83-6c4f-4a62-a165-696756d63903::"
 
 cos = ibm_boto3.resource("s3",
     ibm_api_key_id=COS_API_KEY_ID,
